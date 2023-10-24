bb892e8702e2f595e5979cbd5e66ac198940693a
Abhishek <abhishekmrjbr@outlook.com>
2023-10-24 21:15:15 +0530

    i just add textfield in city_screen.dart file

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class City_screen extends StatefulWidget {
   const City_screen({super.key});
 
@@ -11,22 +12,40 @@ class _City_screenState extends State<City_screen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(image: DecorationImage(image: AssetImage("images/city_background.jpg"),fit: BoxFit.cover)),
-     child: SafeArea(
-       child: Column(
-         children: [
-           Align(
-               alignment: Alignment.topLeft,
-               child: TextButton(onPressed: (){}, child: Icon(Icons.arrow_back_ios))),
-           Container(
-             margin: EdgeInsets.all(15),
-             child: null,
-           ),
-           TextButton(onPressed: (){}, child: Text("Get Weather"))
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("images/city_background.jpg"),
+                fit: BoxFit.cover)),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Align(
+                  alignment: Alignment.topLeft,
+                  child: TextButton(
+                      onPressed: () {}, child: Icon(Icons.arrow_back_ios))),
+              Container(
+                margin: EdgeInsets.all(15),
+                child: TextField(
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Enter City',
+                    icon: Icon(Icons.location_city,color: Colors.white,),
+                    hintStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    borderSide: BorderSide.none),
 
-         ],
-       ),
-     ),
+                  ),
+                ),
+              ),
+              TextButton(onPressed: () {}, child: Text("Get Weather"))
+            ],
+          ),
+        ),
       ),
     );
   }
